e2f30db3f7eee5ce72fd53b1c9ec6e68aeead604
Naielly <naiellyvitoria012@gmail.com>
2025-06-22 13:55:02 -0300

    Adicionando js login

---
+++ src/js/login.js
@@ -0,0 +1,12 @@
+let nome=
+document.getElementById("NOME");
+
+document.getElementById("button")
+.addEventListener("click",function()
+{
+window.location.href= "menu.html";
+{
+    alert(`Olá,${nome.value}! Sua inscrição foi bem sucedida`);
+}
+
+})
